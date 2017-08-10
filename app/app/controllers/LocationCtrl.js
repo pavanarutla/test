aab34eabd8261dd258d06440fa1bb9f6f566546c
Mridul Kashyap <mridulkashyap57@gmail.com>
2017-08-10 11:28:36 +0530

    -_

--- app/app/controllers/LocationCtrl.js
+++ app/app/controllers/LocationCtrl.js
@@ -1,10 +1,13 @@
 'use strict';
 
-app.controller('LocationCtrl', ['$scope', 'NgMap', function($scope, NgMap) {
+app.controller('LocationCtrl', ['$scope', 'NgMap', 'MapService', function($scope, NgMap, MapService) {
+  $scope.dynMarkers = [];
   NgMap.getMap().then(function(map) {
-    console.log(map.getCenter());
-    console.log('markers', map.markers);
-    console.log('shapes', map.shapes);
+    for (var i=0; i<1000; i++) {
+      var latLng = new google.maps.LatLng(MapService.markers()[i].position[0], MapService.markers()[i].position[1]);
+      $scope.dynMarkers.push(new google.maps.Marker({position:latLng}));
+    }
+    // $scope.markerClusterer = new MarkerClusterer(map, $scope.dynMarkers, {});
   });
   $scope.click = function() {
     alert(1);
