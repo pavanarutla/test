5e990a06e0a2e920d1216893a28c555c2a05673f
Naresh Babu <Naresh.babu@ptgindia.com>
2017-08-17 15:41:48 +0530

    -

--- app/app/controllers/FormatsCtrl.js
+++ app/app/controllers/FormatsCtrl.js
@@ -3,16 +3,101 @@ app.controller('formatsCtrl', function ($scope) {
   $scope.showBillboardsData = true;
   $scope.showUnipoleData = false;
   $scope.showdigitalData = false;
+  $scope.showretailData = false;
+  $scope.showtransitData = false;
+  $scope.showairportData = false;
+  $scope.showbusData = false;
+  $scope.showrailData = false; 
+
 
   $scope.showBillboards = function(){
     $scope.showBillboardsData = true;
     $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
   }
 
-  $scope.unipole = function() {
-    $scope.showUnipoleData = true;
+  $scope.unipole = function() {   
     $scope.showBillboardsData = false;
+    $scope.showUnipoleData = true;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
   }
+
+  $scope.showdigital = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = true;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
+  }
+  $scope.showretail = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = true;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
+  }
+
+  $scope.showtransit = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = true;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
+  }
+
+  $scope.showairport = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = true;
+    $scope.showbusData = false;
+    $scope.showrailData = false;
+  }
+  $scope.showbus = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = true;
+    $scope.showrailData = false;
+  }
+
+  $scope.showrail = function(){
+    $scope.showBillboardsData = false;
+    $scope.showUnipoleData = false;
+    $scope.showdigitalData = false;
+    $scope.showretailData = false;
+    $scope.showtransitData = false;
+    $scope.showairportData = false;
+    $scope.showbusData = false;
+    $scope.showrailData = true;
+  }
+
+
+
   //slider
   $scope.slickConfig2Loaded = true;
   $scope.slickConfig2 = {
