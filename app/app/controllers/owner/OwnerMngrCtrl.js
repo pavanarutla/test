f541b4c7fbcc93026984e59239f3fcd0452b1fb0
vasudev baggi <vasudev.baggi@ptgindia.com>
2017-10-18 17:10:46 +0530

    owner-UI

---
+++ app/app/controllers/owner/OwnerMngrCtrl.js
@@ -0,0 +1,14 @@
+app.controller('OwnerMngrCtrl', function ($scope,$mdSidenav,$log) {
+    $scope.toggleLeft = buildToggler('left');
+    
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+})
