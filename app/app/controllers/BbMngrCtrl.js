2d29d3d52e1fa3f610176ff79ea2d5b93a55ae3c
Naresh Babu <Naresh.babu@ptgindia.com>
2017-10-30 16:23:45 +0530

    -

--- app/app/controllers/BbMngrCtrl.js
+++ app/app/controllers/BbMngrCtrl.js
@@ -1,9 +1,11 @@
-app.controller('bbMngrCtrl', function ($scope, $mdDialog, $mdSidenav, $timeout, $location, $rootScope,MapService, $auth, toastr, ContactService, CampaignService, UserService, config) {
+app.controller('bbMngrCtrl', function ($scope, $mdDialog, $mdSidenav, $timeout, $location, $rootScope,MapService, $auth, toastr, ContactService, CampaignService, UserService, LocationService, config) {
 
   if(localStorage.isAuthenticated && localStorage.loggedInUser){
     $rootScope.isAuthenticated = localStorage.isAuthenticated || false;
     $rootScope.loggedInUser = JSON.parse(localStorage.loggedInUser);
   }
+
+  $scope.selectedAreaFilter = null;
 
   // handles traffic layer on map
   $scope.trafficOn = false;
@@ -380,5 +382,26 @@ app.controller('bbMngrCtrl', function ($scope, $mdDialog, $mdSidenav, $timeout, 
 
   $scope.close = function(){
     $mdDialog.hide();
+  }
+  // autocomplete for location search 
+  $scope.autoCompleteArea = function(query) {
+    return LocationService.getAreasWithAutocomplete(query);
+  }
+  $scope.selectedAreaChanged = function(area){
+    //$state.go('/location');
+    localStorage.setItem(areaitem,"area");
+    localStorage.getItem(areaitem);
+
+    console.log(areaitem);
+    $scope.selectedAreaFilter = area;
+    if(area){
+      $scope.mapObj.setCenter({lat: Number(area.lat), lng: Number(area.lng)});
+      var bounds = new google.maps.LatLngBounds();
+      bounds.extend({lat: Number(area.lat), lng: Number(area.lng)});
+      $scope.mapObj.fitBounds(bounds);
+    }
+  }
+  $scope.BrowseBillboards = function(){
+    $location.path('/location');
   }
 });
