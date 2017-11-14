e9da2befbefb5b73615cdba91448292cea76490c
vasudev baggi <vasudev.baggi@ptgindia.com>
2017-11-14 17:06:58 +0530

    Missed file added

---
+++ app/app/controllers/UserProfileCtrl.js
@@ -0,0 +1,37 @@
+app.controller('UserProfileCtrl', function($scope, $stateParams, UserService) {
+  
+  $scope.getLoggedInUserProfile = function(){
+    UserService.getProfile().then(function(result){
+      $scope.userProfile = result;
+    });
+  }
+  $scope.getLoggedInUserProfile();
+
+  $scope.hoardingitems=[
+    {
+      cname:'Flipkart',
+      phone:'(+91) 9878564523',
+      email:'Lorem.ipsum.com',
+      address:'K67/68, Sonal Heavy Indl Est,Lane Extn,Banjara Hills',
+    },
+    {
+    
+      cname:'LG',
+      phone:'(+91) 9878564523',
+      email:'Lorem.ipsum.com',
+      address:'K67/68, Sonal Heavy Indl Est,Lane Extn,Banjara Hills',
+    },
+    {
+      cname:'Iphone',
+      phone:'(+91) 9878564523',
+      email:'Lorem.ipsum.com',
+      address:'K67/68, Sonal Heavy Indl Est,Lane Extn,Banjara Hills',
+    }
+  ];
+
+  $scope.limit= 5;
+  $scope.loadMore = function() {
+    $scope.limit = $scope.items.length
+  };
+
+});
