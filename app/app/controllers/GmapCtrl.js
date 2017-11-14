be0d2732215723a8fc7df91abbcf40563dbeced0
Srikanth Bijalla <srikanthbijalla1990@gmail.com>
2017-11-14 16:36:22 +0530

    changes responsive

--- app/app/controllers/GmapCtrl.js
+++ app/app/controllers/GmapCtrl.js
@@ -386,7 +386,22 @@ app.controller('GmapCtrl',
           }
         });
       };
-
+      ////////////////////////////////////////////////////////////////////////
+      // tablet filters filtersMap
+      
+      $scope.toggleViewAllFilter = function() {
+        $mdSidenav('filtersMobile').toggle();
+      };
+      $scope.mapFilter = function() {
+        $mdSidenav('filtersMap').toggle();
+      };
+      $scope.shortListed = function() {
+        $mdSidenav('shortlistedList').toggle();
+      };
+      $scope.savedCampagin = function() {
+        $mdSidenav('savedCamapgin').toggle();
+      };
+      ////////////////////////////////////////////////////////////////////
       //Suggest Me Dialog 1      
       $scope.suggestionRequest = {};
       $scope.suggestMeRequestSent = false;
