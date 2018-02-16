50bbc0a09196800f05b0a6682f93153c4e61bec2
unknown <hemanthrai47@gmail.com>
2018-02-16 09:36:30 +0530

    gulp test

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require('gulp');
+const minify = require('gulp-uglify');
 
 gulp.task('default',function(){
     console.log('testing the gulp runner');
 });
+gulp.task('compress',function(){
+    gulp.src('node_modules/angular/angular.js')
+    .pipe(minify())
+    .pipe(gulp.dest('dist/scripts'))
+})
