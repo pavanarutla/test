8d67db6c16e52cc235d99b9e572260609760df3e
hemanthrai47 <hemanth.ednith@gmail.com>
2018-02-16 21:25:21 +0530

    gulp setup updated

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,11 +1,27 @@
 const gulp = require('gulp');
 const minify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const clean = require('del');
 
-gulp.task('default',function(){
+//tasks
+const app = {
+    clean: 'clean',
+    compress : 'compress'
+}
+
+gulp.task('run',['app.clean','app.compress'],function(){
     console.log('testing the gulp runner');
 });
-gulp.task('compress',function(){
-    gulp.src('node_modules/angular/angular.js')
-    .pipe(minify())
-    .pipe(gulp.dest('dist/scripts'))
-})
+
+gulp.task('app.clean',function(){
+    clean(["dist/scripts/*.min.js"])   
+});
+
+gulp.task('app.compress',function(){
+    var controllers = ['app/controllers/admin/AdminLocationCtrl.js']
+    gulp.src(controllers)
+        .pipe(minify())
+        .pipe(concat("app.min.js"))
+        .pipe(gulp.dest('dist/scripts'))
+});
