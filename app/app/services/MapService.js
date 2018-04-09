b7ece49426332530178ad35bba9a9a7dd337d7ae
Mridul Kashyap <mridulkashyap57@gmail.com>
2018-04-09 11:08:20 +0530

    -

--- app/app/services/MapService.js
+++ app/app/services/MapService.js
@@ -23,7 +23,7 @@ app.factory('MapService', ['$http', '$q', 'config', function ($http, $q, config)
     },
     getshortListProduct: function (userMongoId) {
       var dfd = $q.defer();
-      $http.get(config.apiPath + '/shortlistedProducts/' + userMongoId).success(dfd.resolve).error(dfd.reject);
+      $http.get(config.apiPath + '/shortlistedProducts').success(dfd.resolve).error(dfd.reject);
       return dfd.promise;
     },
     deleteShortlistedProduct: function (userMongoId, productId) {
