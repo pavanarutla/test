af60071747d3bf3cd2ecb45745768116865eeb4a
Bijjala <srikanthbijjala1990@gmail.com>
2019-03-02 10:11:12 +0530

    srikath-Homepage

--- app/app/app.js
+++ app/app/app.js
@@ -755,7 +755,7 @@ app.run(
         // Get all URL parameter
         $rootScope.currentTitle = transition.to().title;
         $rootScope.currStateName = transition.to().name;
-        if ((transition.to().name == "index.location" || transition.to().name == "index.metro")&& $auth.isAuthenticated()) {
+        if ((transition.to().name == "index.location" || transition.to().name == "index.metro" || transition.to().name == "index.suggest.product-detail")&& $auth.isAuthenticated()) {
           $rootScope.footerhide = true;
         }
         else {
