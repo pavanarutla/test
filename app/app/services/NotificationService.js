1185e5aa99fa3deaeb88507a363ae0e5d1731b99
pavanarutla <pavanarutla@users.noreply.github.com>
2019-03-04 22:30:48 +0530

    notifications added

--- app/app/services/NotificationService.js
+++ app/app/services/NotificationService.js
@@ -1,16 +1,21 @@
-app.service('NotificationService', ['$http', '$q', 'config', 
-  function($http, $q, config){
-    return {
-      getAllNotifications: function(last_notif){
-        var dfd = $q.defer();
-        $http.get(config.apiPath + '/all-notifications/last-notif/' + last_notif, {skipInterceptor: true}).success(dfd.resolve).error(dfd.reject);
-        return dfd.promise;
-      }, 
-      updateNotifRead:  function(notifId){
-        var dfd = $q.defer();
-        $http.get(config.apiPath + '/update-notification-read/' + notifId, {skipInterceptor: true}).success(dfd.resolve).error(dfd.reject);
-        return dfd.promise;
-      }, 
+app.service('NotificationService', ['$http', '$q', 'config',
+    function ($http, $q, config) {
+        return {
+            getAllNotifications: function (last_notif) {
+                var dfd = $q.defer();
+                $http.get(config.apiPath + '/all-notifications/last-notif/' + last_notif, {skipInterceptor: true}).success(dfd.resolve).error(dfd.reject);
+                return dfd.promise;
+            },
+            updateNotifRead: function (notifId) {
+                var dfd = $q.defer();
+                $http.get(config.apiPath + '/update-notification-read/' + notifId, {skipInterceptor: true}).success(dfd.resolve).error(dfd.reject);
+                return dfd.promise;
+            },
+            viewUserNotification: function () {
+                var dfd = $q.defer();
+                $http.get(config.apiPath + '/get-notifications', {skipInterceptor: true}).success(dfd.resolve).error(dfd.reject);
+                return dfd.promise;
+            },
+        }
     }
-  }
 ]);
