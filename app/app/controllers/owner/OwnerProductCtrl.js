fcd7f78e8a93c1e22ddde1a9444c997e2cbf4a97
Vasudev <vasudev.baggi@ptgindia.com>
2019-03-06 18:11:21 +0530

    Calender-Owner

--- app/app/controllers/owner/OwnerProductCtrl.js
+++ app/app/controllers/owner/OwnerProductCtrl.js
@@ -160,28 +160,38 @@ $scope.applymethod=function(product){
         cancelLabel: 'Cancel',
         customRangeLabel: 'Custom range'
     },
-    isInvalidDate : function(dt){
-      for(var i=0; i < $scope.unavailalbeDateRanges.length; i++){
-        if(moment(dt) >= $scope.unavailalbeDateRanges[i].start && moment(dt) <= $scope.unavailalbeDateRanges[i].end){
+    isInvalidDate: function (dt) {
+      for (var i = 0; i < $scope.unavailalbeDateRanges.length; i++) {
+          if (moment(dt) >= moment($scope.unavailalbeDateRanges[i].booked_from) && moment(dt) <= moment($scope.unavailalbeDateRanges[i].booked_to)) {
+              return true;
+          }
+      }
+      if(moment(dt) < moment()){
           return true;
-        }
       }
-    },
-    isCustomDate: function(dt){
-      for(var i = 0; i < $scope.unavailalbeDateRanges.length; i++){
-        if(moment(dt) >= $scope.unavailalbeDateRanges[i].start && moment(dt) <= $scope.unavailalbeDateRanges[i].end){
-          if(moment(dt).isSame($scope.unavailalbeDateRanges[i].start, 'day')){
-            return ['red-blocked', 'left-radius'];
+  },
+  isCustomDate: function (dt) {
+      for (var i = 0; i < $scope.unavailalbeDateRanges.length; i++) {
+          if (moment(dt) >= moment($scope.unavailalbeDateRanges[i].booked_from) && moment(dt) <= moment($scope.unavailalbeDateRanges[i].booked_to)) {
+              if (moment(dt).isSame(moment($scope.unavailalbeDateRanges[i].booked_from), 'day')) {
+                  return ['red-blocked', 'left-radius'];
+              } else if (moment(dt).isSame(moment($scope.unavailalbeDateRanges[i].booked_to), 'day')) {
+                  return ['red-blocked', 'right-radius'];
+              } else {
+                  return 'red-blocked';
+              }
           }
-          else if(moment(dt).isSame($scope.unavailalbeDateRanges[i].end, 'day')){
-            return ['red-blocked', 'right-radius'];
-          }
-          else{
-            return 'red-blocked';
-          }
-        }
       }
-    },
+      if(moment(dt) < moment()){
+          return 'gray-blocked';
+      }
+  },
+  eventHandlers: {
+      'apply.daterangepicker': function(ev, picker) { 
+          //selectedDateRanges = [];
+          console.log(ev);
+      }
+  }
   };
   $scope.inventoryListOpts = {
     multipleDateRanges: true,
