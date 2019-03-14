9426aeec7528402eb3f6faa7e780b338c6fb6233
pavanarutla <pavan.arutla@gmail.com>
2019-03-14 15:40:45 +0530

    changes

--- app/app/services/admin/CampaignService.js
+++ app/app/services/admin/CampaignService.js
@@ -75,7 +75,7 @@ app.service('AdminCampaignService',
                     finalizeCampaignByAdmin: function (campaignId, flag, GST) {
                         var dfd = $q.defer();
                         $http.get(config.apiPath + '/quote-campaign/' + campaignId + '/' + flag + '/' + GST).success(dfd.resolve).error(dfd.reject);
-                        console.log(config.apiPath + '/quote-campaign/' + campaignId + '/' + flag + '/' + GST);                       
+                        // console.log(config.apiPath + '/quote-campaign/' + campaignId + '/' + flag + '/' + GST);                       
                         return dfd.promise;
                     },
                     confirmCampaignBooking: function (campaignId) {
